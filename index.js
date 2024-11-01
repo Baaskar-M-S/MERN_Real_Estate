9c122ddca8956758dac65bffd4e8ebf3c4e09642
Baaskar MS <baaskar12308@gmail.com>
2024-11-01 18:55:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,36 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/DB');
 const app = express();
-const crypto = require('crypto');
-const nodemailer = require('nodemailer'); // For sending email
-const twilio = require('twilio'); // For sending SMS
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Health Check Route
+connectDB();
+
 app.get('/', (req, res) => {
   res.send('Server is alive!');
 });
 
-// Routes
 const userRoute = require('./routes/userRoute');
 const propertyRoute = require('./routes/propertyRoute');
 
-app.use('/', userRoute);
-app.use('/', propertyRoute);
+app.use('/api/users', userRoute);
+app.use('/api/properties', propertyRoute);
 
-const PORT = process.env.PORT || 3000; // Ensure default port is set
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
